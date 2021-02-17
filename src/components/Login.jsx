63cf0c8cb53b09a79308a7122d05156c2067ca96
José María Muñoz Quijada <JoseMariaMQ@users.noreply.github.com>
2021-02-17 22:19:02 +0100

    Capturar errores registro firebase

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -5,26 +5,36 @@ const Login = () => {
 
     const [email, setEmail] = useState("")
     const [pass, setPass] = useState("")
+    const  [msgError, serMsgError] = useState(null)
 
     const registrarUsuario = (e) => {
         e.preventDefault()
-        try {
-            auth.createUserWithEmailAndPassword(email, pass)
-            alert("Usuario registrado")
-        } catch (e) {
-            console.log(e)
-        }
+        auth.createUserWithEmailAndPassword(email, pass)
+            .then(r => alert("Usuario registrado"))
+            .catch (e => {
+                if (e.code === 'auth/invalid-email') serMsgError('Formato de Email incorrecto')
+                if (e.code === 'auth/weak-password') serMsgError('La password debe tener 6 mínimo caracteres')
+            })
     }
 
     return (
         <div className="row mt-5">
             <div className="col"/>
             <div className="col">
-                <form onSubmit={registrarUsuario} className="from-group">
-                    <input onChange={(e) => {setEmail(e.target.value)}} className="form-control" placeholder="Introcude el Email" type="text"/>
+                <form onSubmit={registrarUsuario} className="from-group mb-4">
+                    <input onChange={(e) => {setEmail(e.target.value)}} className="form-control" placeholder="Introcude el Email" type="email"/>
                     <input onChange={(e) => {setPass(e.target.value)}} className="form-control mt-4" placeholder="Introduce la Password" type="password"/>
                     <input className="btn btn-dark btn-block mt-4" value="Registrar Usuario" type="submit"/>
                 </form>
+                {
+                    msgError != null ?
+                        (
+                            <div>{msgError}</div>
+                        ) :
+                        (
+                            <span></span>
+                        )
+                }
             </div>
             <div className="col"/>
         </div>
